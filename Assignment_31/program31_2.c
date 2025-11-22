afca41ef3c579c4d6f3706873106e0f6818c939a
ankurtakale <takaleankur@gmail.com>
2025-11-22 23:43:33 +0530

    initial commit

---
+++ Assignment_31/program31_2.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0, iCnt = 1;
+
+    if(iRow != iCol)
+    {
+        printf("Provide a valid input\n");
+        printf("To print this pattern you must provide equal number of columns and rows\n");
+
+        return;
+    }
+    
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = iCol; i <= j; j--)
+        {
+            printf("*\t");
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter rows : ");
+    scanf("%d",&iValue1);
+
+    printf("Enter columns : ");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+    return 0;
+}
